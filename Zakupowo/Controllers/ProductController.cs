d9532a39283448311f6d8f7ee06d8b14145e441a
Kacper <87197@student.pb.edu.pl>
2024-11-09 12:27:56 +0100

    Wyświetlanie produktów

---
+++ Zakupowo/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Zakupowo.Models;
+
+namespace Zakupowo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET
+        private ZakupowoDbContext _context = new ZakupowoDbContext();
+        public ActionResult ProductList()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
