431c6908982eeac7043aff65fddee39079ff71d9
Kacper <87197@student.pb.edu.pl>
2024-12-04 13:51:00 +0100

    Dodane motywy

---
+++ Zakupowo/Controllers/ThemeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Zakupowo.Controllers;
+
+public class ThemeController : Controller
+{
+    public ActionResult ChangeTheme(string theme)
+    {
+        Session["SelectedTheme"] = theme;
+        return Redirect(Request.UrlReferrer?.ToString() ?? "/");
+    }
+}
